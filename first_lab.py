170f76b7234dde96b0afc837545b53bf78e6c077
uliassshik <113136344+uliassshik@users.noreply.github.com>
2022-10-11 18:29:23 +0300

    Update first_lab.py

--- first_lab.py
+++ first_lab.py
@@ -4,12 +4,15 @@
 
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# И
 # Импорт пакетов
+
 import math
 import numpy
 import matplotlib.pyplot as mpp
 
 # Эта программа рисует график функции, заданной выражением ниже
+# Главная функция
 
 if __name__=='__main__':
     arguments = numpy.arange(0, 200, 0.1)
