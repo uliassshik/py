e7ec58638138514f5b9e8c882c9965e4b605e653
uliassshik <113136344+uliassshik@users.noreply.github.com>
2022-12-18 14:29:37 +0300

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -6,10 +6,12 @@ def isPrime(a):
     for i in range (2,x+1):
         if y%i==0:
             return False
-        
         else:
             continue
+    if y<=1:
+        return False
     return True
-
+print(isPrime(0))  #False   
+print(isPrime(1))  #False   
 print(isPrime(144))  #False     
 print(isPrime(1103))    #True    
